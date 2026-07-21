c47d94491ebe25ec52818a9ddbda4a547b7b2ab6
smajlooo <contact@smajlo.com>
2026-07-21 20:46:22 +0200

    Interface for shape created

---
+++ CSTetris.Lib/IShape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSTetris.Lib
+{
+    public interface IShape
+    {
+        void Draw(int x, int y);
+
+        void Move();
+
+        void Rotate();
+    }
+}
